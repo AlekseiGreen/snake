3c271990b4e23279db48ad7b2b2536448e1d310e
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2025-01-27 23:26:17 +1000

    Add files via upload

---
+++ src/Box.jsx
@@ -0,0 +1,23 @@
+import './Box.css';
+
+
+function Box(){
+    return(
+        <>
+            <div className="box">
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                <div className="cell"></div>
+                
+            </div>
+        </>
+    )
+}
+
+export default  Box;
