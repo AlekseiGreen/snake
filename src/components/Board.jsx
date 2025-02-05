59cb5c46a78459d85437ef2cc36c22554fda75af
AlekseiGreen <120323580+AlekseiGreen@users.noreply.github.com>
2025-02-05 22:40:15 +1000

    Add files via upload

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Square from './Square';
 import "./Board.css"
+import ImgBG from "../assets/rm-mind.jpg";
 
 
 const Board = () => {
@@ -40,23 +41,24 @@ const Board = () => {
         <div>
             <h2>{winner ? `Победитель: ${winner}` : `Следующий ход: ${isXNext ? 'X' : 'O'}`}</h2>
             <div className="board-main">
-                <div className="board-row">
+                {/* <div className="board-row"> */}
                     <Square value={squares[0]} onClick={()=> handleClick(0)}></Square>
                     <Square value={squares[1]} onClick={()=> handleClick(1)}></Square>
                     <Square value={squares[2]} onClick={()=> handleClick(2)}></Square>
-                </div>
-                <div  className="board-row">
+                {/* </div>
+                <div  className="board-row"> */}
                     <Square value={squares[3]} onClick={()=> handleClick(3)}></Square>
                     <Square value={squares[4]} onClick={()=> handleClick(4)}></Square>
                     <Square value={squares[5]} onClick={()=> handleClick(5)}></Square>
-                </div>
-                <div  className="board-row">
+                {/* </div>
+                <div  className="board-row"> */}
                     <Square value={squares[6]} onClick={()=> handleClick(6)}></Square>
                     <Square value={squares[7]} onClick={()=> handleClick(7)}></Square>
                     <Square value={squares[8]} onClick={()=> handleClick(8)}></Square>
-                </div>
+                {/* </div> */}
             </div>
             <button onClick={handleRestart}>Перезапустить игру</button>
+            <img src={ImgBG} alt="ImgBG" />
 
 
         </div>
